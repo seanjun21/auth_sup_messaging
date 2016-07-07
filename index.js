2c4167c9c4d084795ad95ac1dda5af01cbd6f9da
Sierra <sierragregg@Gmail.com>
2016-07-07 19:56:08 +0000

    Added additional comments

--- index.js
+++ index.js
@@ -15,27 +15,32 @@ var jsonParser = bodyParser.json();
 app.get('/users', function(request, response) {
     User.find(function(error, users) {
         if (error) {
+            //internal server error
             response.sendStatus(500);
             return;
         }
+        //if no error, response will return the users array in json
         response.json(users);
     });
 });
 
-// POST request for array of users
+// POST request for single user
 app.post('/users', jsonParser, function(request, response) {
+    // if username string is empty or undefined, return 422 error
     if (!request.body.username) {
         response.status(422).json({
             message: 'Missing field: username'
         });
         return;
     }
+    // if username is not a string then return incorrect type error
     if (typeof request.body.username !== 'string') {
         response.status(422).json({
             message: 'Incorrect field type: username'
         });
         return;
     }
+    // if POST req is valid, create new user
     User.create({
         username: request.body.username
     }, function(error, user) {
@@ -43,6 +48,7 @@ app.post('/users', jsonParser, function(request, response) {
             response.sendStatus(422);
             return;
         }
+        // response with 'created' message and path of new user
         response.status(201).header('Location', '/users/' + user._id).json({});
     });
 });
@@ -53,36 +59,44 @@ app.get('/users/:userID', function(request, response) {
     User.find({
         _id: request.params.userID
     }, function(error, user) {
+        // check if user[0] in returned array is falsey 
         if (!user[0]) {
+            // return 404 error message if specified user does not exist
             response.status(404).json({
                 message: "User not found"
             });
             return;
         }
+        // returns OK status and user that was queried in response
         response.json(user[0]);
     });
 });
 
 // PUT request to add or edit a user.
 app.put('/users/:userID', jsonParser, function(request, response) {
+    // check that username provided is not an empty field
     if (!request.body.username) {
         response.status(422).json({
             message: 'Missing field: username'
         });
         return;
     }
+    // check that username provided is a string
     if (typeof request.body.username !== 'string') {
         response.status(422).json({
             message: 'Incorrect field type: username'
         });
         return;
     }
+    // find requested user and update username in user document
     User.findOneAndUpdate({
         _id: request.params.userID
     }, {
         username: request.body.username
     }, function(error, user) {
+        // if there is not user document with specified user ID
         if (!user) {
+            // creates a new user at specified user ID with provided username
             var newUser = {
                 _id: request.params.userID,
                 username: request.body.username
@@ -100,15 +114,18 @@ app.put('/users/:userID', jsonParser, function(request, response) {
 
 // DELETE request to remove user by user ID
 app.delete('/users/:userID', jsonParser, function(request, response) {
+    // find user document by user ID and remove document
     User.findByIdAndRemove({
         _id: request.params.userID
     }, function(error, user) {
+        // if user doc does not exist, return 404 error
         if (!user) {
             response.status(404).json({
                 message: 'User not found'
             });
             return;
         }
+        // if user successfully removed, return status OK with empty object
         response.json({});
     });
 });
@@ -118,6 +135,7 @@ app.delete('/users/:userID', jsonParser, function(request, response) {
 // GET request for messages to/from a user w/ corresponding ObjectID
 app.get('/messages', function(request, response) {
 
+    // depending on specified fields in request.query, will return array of messages matching the fields from request. Request.query will contain the values in query string in an object. May include no values, or 'to', 'from', or both. These fields have only the userId, we use populate to access the information stored only in the user document.
     Message.find(request.query).populate('from to').exec(function(error, messages) {
         if (error) {
             response.sendStatus(500);
@@ -129,10 +147,6 @@ app.get('/messages', function(request, response) {
 
 // POST request for messages
 app.post('/messages', jsonParser, function(request, response) {
-    var toFieldisValidID = true;
-    var fromFieldisValidID = true;
-
-
 
     // No text for the message
     if (!request.body.text) {
@@ -184,13 +198,14 @@ app.post('/messages', jsonParser, function(request, response) {
                         message: 'Incorrect field value: to'
                     });
                 }
-                // else we know both fields are valid users, create the message record
+                // else we know both fields are valid users with appropriate input type and message text is supplied. Will proceed and create the message document.
                 else {
                     Message.create(request.body, function(error, message) {
                         if (error) {
                             response.sendStatus(500);
                             return;
                         }
+                        // if successfully created message, send CREATED status and path of message
                         response.status(201).header('Location', '/messages/' + message._id).json({});
                     });
                 }
@@ -207,6 +222,7 @@ app.get('/messages/:messageId', function(request, response) {
             response.status(404).json({message: 'Message not found'});
             return;
         }
+        // return message that was fetched
         response.json(message);
     });
 });
@@ -228,7 +244,7 @@ var runServer = function(callback) {
 
 if (require.main === module) {
     runServer();
-};
+}
 
 exports.app = app;
 exports.runServer = runServer;
